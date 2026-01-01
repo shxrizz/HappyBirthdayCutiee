95cd8d0016f8bbf3b2b66a52f2276150566629a8
shxrizz <stackerzshaurya@gmail.com>
2026-01-01 22:36:37 +0530

    Change button text on completion

--- cause.js
+++ cause.js
@@ -76,7 +76,7 @@ function displayNewReason() {
                 duration: 0.5,
                 ease: "elastic.out",
                 onComplete: () => {
-                    shuffleButton.textContent = "Enter Our Storylane 💫";
+                    shuffleButton.textContent = "Something more here💫";
                     shuffleButton.classList.add('story-mode');
                     shuffleButton.addEventListener('click', () => {
                         gsap.to('body', {
